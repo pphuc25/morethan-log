22ca692ccdd6600b76c3ad7226fed5b2c72756df
pphuc25 <phanphuc1100@gmail.com>
2023-05-15 23:40:31 +0700

    docs: testing

--- site.config.js
+++ site.config.js
@@ -38,35 +38,35 @@ const CONFIG = {
     pageId: 'https://rounded-limburger-0f8.notion.site/9e3d9b16f1f54636806e1e2feb88e2b5?v=613dbaefea9e42959f0da0ce4091b153'
   },
 
-  // plugin configuration (optional)
-  googleAnalytics: {
-    enable: false,
-    config: {
-      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
-    },
-  },
-  googleSearchConsole: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
-    },
-  },
-  utterances: {
-    enable: true,
-    config: {
-      repo: "morethanmin/morethan-log",
-      "issue-term": "og:title",
-      label: "💬 Utterances",
-    },
-  },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
-    },
-  },
-  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+//   plugin configuration (optional)
+//   googleAnalytics: {
+//     enable: false,
+//     config: {
+//       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+//     },
+//   },
+//   googleSearchConsole: {
+//     enable: false,
+//     config: {
+//       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+//     },
+//   },
+//   utterances: {
+//     enable: true,
+//     config: {
+//       repo: "pphuc25/morethan-log",
+//       "issue-term": "og:title",
+//       label: "💬 Utterances",
+//     },
+//   },
+//   cusdis: {
+//     enable: false,
+//     config: {
+//       host: "https://cusdis.com",
+//       appid: "", // Embed Code -> data-app-id value
+//     },
+//   },
+//   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 }
 
 module.exports = { CONFIG }
